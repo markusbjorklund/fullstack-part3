668680b5c4eec7b1ba4071eed9f5607184dd3967
Markus Bjorklund <markus.bjorklund@gmail.com>
2020-10-07 23:40:23 +0300

    3.4 done

--- index.js
+++ index.js
@@ -32,12 +32,19 @@ app.get('/api/persons', (req, res) => {
 app.get('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
   const person = persons.find(person => person.id === id)
-  
+
   if (person) {
     response.json(person)
   } else {
     response.status(404).end()
   }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
 })
 
 app.get('/info', (req, res) => {
