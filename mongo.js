5c7bece4c6142838df8575f6c745a21c5b3bc971
Markus Bjorklund <markus.bjorklund@gmail.com>
2020-10-12 17:55:14 +0300

    3.12 final

--- mongo.js
+++ mongo.js
@@ -22,24 +22,27 @@ const Person = mongoose.model('Person', personSchema)
 
 if (process.argv.length === 3) {
   Person.find({}).then(persons => {
-    console.log('phonebook:')
+    console.log('Phonebook:')
     persons.forEach(person => {
       console.log(`${person.name} ${person.number}`)
     })
     mongoose.connection.close();
   })
-
 }
 
-if (process.argv.length === 5) {
+else if (process.argv.length === 5) {
   const name = process.argv[3];
   const number = process.argv[4];
   const person = new Person({
     name, number
   })
-
   person.save(phonebook => {
     console.log(`added ${person.name} number ${person.number}`);
     mongoose.connection.close();
   })
+}
+
+else {
+  console.log(`Please provide both a name and a number`)
+  mongoose.connection.close();
 }
