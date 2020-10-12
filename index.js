64b616bedd98909bd3a7047a6979f6587a7c655e
Markus Bjorklund <markus.bjorklund@gmail.com>
2020-10-12 12:26:45 +0300

    created heroku

--- index.js
+++ index.js
@@ -92,6 +92,8 @@ app.get('/info', (req, res) => {
   res.send('<p>Phonebook has info for ' + persons.length + ' people</p> <p>' + Date() + '</p>')
 })
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+const PORT = preocess.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
+
