0ed4dff1258726ea6c1d862c47ff71ac1a45a45a
Markus Bjorklund <markus.bjorklund@gmail.com>
2020-10-13 09:32:27 +0300

    3.15 and 3.16 done

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
 const Person = require('./models/persons')
+const { nextTick } = require('process')
 
 app.use(express.json())
 app.use(cors())
@@ -14,43 +15,35 @@ morgan.token('body', (req, res) => JSON.stringify(req.body));
 
 app.use(morgan(':method :url :status :res[content-length] :response-time ms :body'));
 
-let persons = [
-  {
-    "id": 1,
-    "name": "Arto Hellas",
-    "number": "040-123456"
-  },
-  {
-    "id": 2,
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523"
-  },
-  {
-    "id": 3,
-    "name": "Dan Abramov",
-    "number": "12-43-234345"
-  },
-  {
-    "id": 4,
-    "name": "Mary Popps a dieck",
-    "number": "39-23-6423122"
-  }
-]
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
 
-app.delete('/api/persons/:id', (request, response) => {
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+  next(error)
+}
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(errorHandler)
+
+app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndRemove(request.params.id)
     .then(phonebook => {
       response.status(204).end()
     })
+    .catch(error => next(error))
 })
-
 
 const makeID = () => {
   const randomID = Math.floor(Math.random() * 999)
   return randomID
 }
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
   const person = new Person({
@@ -59,9 +52,22 @@ app.post('/api/persons', (request, response) => {
     id: makeID(),
   })
 
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: 'Name or number is missing'
+    })
+  }
+
+  if (persons.some(person => person.name === body.name)) {
+    return response.status(400).json({
+      error: 'Name already exists in phonebook'
+    })
+  }
+
   person.save().then(savedPerson => {
     response.json(savedPerson)
   })
+  .catch(error => next(error))
 })
 
 app.get('/api/persons', (request, response) => {
@@ -70,7 +76,7 @@ app.get('/api/persons', (request, response) => {
   })
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id)
     .then(phonebook => {
       if (phonebook) {
@@ -78,14 +84,13 @@ app.get('/api/persons/:id', (request, response) => {
       }
       else { response.status(404).end() }
     })
-    .catch(error => {
-      console.log(error)
-      response.status(400).send({ error: 'malformatted id' })
-    })
+    .catch(error => next(error))
 })
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
-
