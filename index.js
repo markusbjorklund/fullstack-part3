c66d8483aa0d429bd0a97b31e5d9f5fbafec2b6f
Markus Bjorklund <markus.bjorklund@gmail.com>
2020-10-13 19:57:05 +0300

    3.22 done~

--- index.js
+++ index.js
@@ -1,51 +1,13 @@
 require('dotenv').config()
-const http = require('http')
 const express = require('express')
 const morgan = require('morgan')
-const cors = require('cors')
 const app = express()
 const Person = require('./models/persons')
-const { nextTick } = require('process')
-const { response, request } = require('express')
-const { errorMonitor } = require('stream')
-app.use(cors())
-morgan.token('body', (req, res) => JSON.stringify(req.body));
-app.use(morgan(':method :url :status :res[content-length] :response-time ms :body'));
+morgan.token('body', (request) => JSON.stringify(request.body))
+app.use(morgan(':method :url :status :res[content-length] :response-time ms :body'))
 
 app.use(express.static('build'))
 app.use(express.json())
-
-const makeID = () => {
-  const randomID = Math.floor(Math.random() * 999)
-  return randomID
-}
-
-app.post('/api/persons', (request, response, next) => {
-  const body = request.body
-
-  if (body.name === undefined) {
-    return response.status(400).json({ error: 'Insert a name' })
-  }
-
-  if (body.number === undefined) {
-    return response.status(400).json({ error: 'Insert a number' })
-  }
-
-  const person = new Person({
-    name: body.name,
-    number: body.number,
-    id: makeID(),
-  })
-
-  person.save().then(savedPerson => {
-    response.json(savedPerson)
-  })
-    .catch(error => next(error))
-})
-
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'Unknown endpoint' })
-}
 
 const errorHandler = (error, request, response, next) => {
   console.error(error.message)
@@ -59,10 +21,30 @@ const errorHandler = (error, request, response, next) => {
   next(error)
 }
 
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'Unknown endpoint' })
+}
+
+app.post('/api/persons', (request, response, next) => {
+  const body = request.body
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
+
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
+    .catch(error => next(error))
+})
+
 app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndRemove(request.params.id)
     .then(phonebook => {
-      response.status(204).end()
+      if (phonebook) {
+        response.json(phonebook)
+      }
+      else { response.status(204).end() }
     })
     .catch(error => next(error))
 })
@@ -94,7 +76,7 @@ app.put('/api/persons/:id', (request, response, next) => {
 
   Person.findByIdAndUpdate(request.params.id, person, { new: true })
     .then(updatedPerson => {
-      if (phonebook) {
+      if (updatedPerson) {
         response.json(updatedPerson)
       }
       else { response.status(204).end() }
@@ -108,8 +90,8 @@ app.get('/info', (request, response) => {
   })
 })
 
-app.use(errorHandler)
 app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
